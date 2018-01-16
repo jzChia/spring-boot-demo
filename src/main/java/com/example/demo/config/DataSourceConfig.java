a6bfbbc57aa99784a01e5a44d7a81d87b8e88bb9
JZ_CHIA <zhuxi145@163.com>
2018-01-16 13:52:44 +0800

    r

--- src/main/java/com/example/demo/config/DataSourceConfig.java
+++ src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -1,7 +1,33 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
 /**
  * @author jiazhijie on 17/11/2.
  */
+@Configuration
 public class DataSourceConfig {
+
+    @Bean(name = "primaryDataSource")
+    @Qualifier("primaryDataSource")
+    @Primary
+    @ConfigurationProperties(prefix="spring.datasource.primary")
+    public DataSource primaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "secondaryDataSource")
+    @Qualifier("secondaryDataSource")
+    @ConfigurationProperties(prefix="spring.datasource.secondary")
+    public DataSource secondaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
 }
